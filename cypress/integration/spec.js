fa8f5669c99306d9da4a2925063c3469ed3e6ea1
Kyrsten Kelly <kyrsten@udacity.com>
2020-05-21 17:21:07 -0500

    Clean up test code

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,33 +1,23 @@
-const testUrl = 'https://classroom.udacity.com';
-const route = {
-  url: '**/graphql',
-  method: 'POST'
-};
-const numCalls = 4;
-
 describe('Our website', () => {
   beforeEach(() => {
     cy.setCookie('_jwt', '< add your JWT here >');
 
     cy.server();
-    cy.route(route).as('apiRoute');
+    cy.route({
+      url: '**/graphql',
+      method: 'POST'
+    }).as('apiRoute');
   });
 
   it(`Has no accessibility violations`, () => {
     cy.visit('https://classroom.udacity.com/me');
 
-    let i = 0;
     // Also try waiting for the requests one at a time
-    // while (i < numCalls) {
-    //   cy.wait('@apiRoute');
-    //   i++;
-    // }
-    const waitArray = [];
-    while (i < numCalls) {
-      waitArray.push('@apiRoute');
-      i++;
-    }
-    cy.wait(waitArray);
+    cy.wait('@apiRoute');
+    cy.wait('@apiRoute');
+    cy.wait('@apiRoute');
+    cy.wait('@apiRoute');
+    // cy.wait(['@apiRoute', '@apiRoute', '@apiRoute', '@apiRoute']);
 
     expect(true);
   });
